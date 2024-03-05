c36757e2400424e7c730b538ff30179e747f4fbe
mDestaFadilah <desta.08b@gmail.com>
2024-03-06 04:06:51 +0700

    view example

--- Shortly.Client/obj/Debug/net6.0/Shortly.Client.AssemblyInfo.cs
+++ Shortly.Client/obj/Debug/net6.0/Shortly.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shortly.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a3e8f01f41e8e12ae82a7a6486350a06af2461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccaedfe5250c775cf1c1ef93921cd438e5f16eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shortly.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shortly.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
